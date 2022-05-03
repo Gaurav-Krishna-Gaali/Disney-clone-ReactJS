86a1e71fdb717297b3858504b85c9e01b8f3c4c5
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-05-03 14:28:55 +0530

    Sign out Feature Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { auth, provider } from "../features/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectUserName,
-  selectUserEmail,
+  setSignOutState,
   selectUserPhoto,
   setUserLoginDetails,
 } from "../features/users/userSlice";
@@ -16,6 +17,8 @@ const Header = (props) => {
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
 
+  // Used to push the user credentials from header component to
+  //  home component via history
   useEffect(() => {
     auth.onAuthStateChanged(async (user) => {
       if (user) {
@@ -26,14 +29,26 @@ const Header = (props) => {
   }, [userName]);
 
   const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        setUser(result.user);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          history.push("/");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    }
   };
 
   const setUser = (user) => {
@@ -81,12 +96,12 @@ const Header = (props) => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          {/* <SignOut> */}
-          <UserImg src={userPhoto} alt={userName} />
-          {/* <DropDown>
+          <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
               <span onClick={handleAuth}>Sign out</span>
-            </DropDown> */}
-          {/* </SignOut> */}
+            </DropDown>
+          </SignOut>
         </>
       )}
     </Nav>
@@ -207,42 +222,42 @@ const UserImg = styled.img`
   height: 100%;
 `;
 
-// const DropDown = styled.div`
-//   position: absolute;
-//   top: 48px;
-//   right: 0px;
-//   background: rgb(19, 19, 19);
-//   border: 1px solid rgba(151, 151, 151, 0.34);
-//   border-radius: 4px;
-//   box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
-//   padding: 10px;
-//   font-size: 14px;
-//   letter-spacing: 3px;
-//   width: 100px;
-//   opacity: 0;
-// `;
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  opacity: 0;
+  width: 100px;
+`;
 
-// const SignOut = styled.div`
-//   position: relative;
-//   height: 48px;
-//   width: 48px;
-//   display: flex;
-//   cursor: pointer;
-//   align-items: center;
-//   justify-content: center;
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
 
-//   ${UserImg} {
-//     border-radius: 50%;
-//     width: 100%;
-//     height: 100%;
-//   }
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
 
-//   &:hover {
-//     ${DropDown} {
-//       opacity: 1;
-//       transition-duration: 1s;
-//     }
-//   }
-// `;
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
 
 export default Header;
